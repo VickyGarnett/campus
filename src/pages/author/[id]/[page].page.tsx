83421d1c418f5d42f667405b4e7844edc7fbbeab
Stefan Probst <stefanprobst@kolabnow.com>
2021-06-27 10:07:37 +0200

    style: format author metadata

--- src/pages/author/[id]/[page].page.tsx
+++ src/pages/author/[id]/[page].page.tsx
@@ -12,8 +12,13 @@ import type { Person as PersonData } from '@/api/cms/person'
 import { getPersonById, getPersonIds } from '@/api/cms/person'
 import type { PostPreview } from '@/api/cms/post'
 import { getPostPreviewsByAuthorId } from '@/api/cms/queries/post'
+import { Svg as EmailIcon } from '@/assets/icons/email.svg'
+import { Svg as GlobeIcon } from '@/assets/icons/globe.svg'
+import { Svg as OrcidIcon } from '@/assets/icons/orcid.svg'
+import { Svg as TwitterIcon } from '@/assets/icons/twitter.svg'
 import { getPageRange, paginate } from '@/cms/paginate'
 import type { Page } from '@/cms/paginate'
+import { Icon } from '@/common/Icon'
 import { PageContent } from '@/common/PageContent'
 import { Pagination } from '@/common/Pagination'
 import { getLocale } from '@/i18n/getLocale'
@@ -24,6 +29,7 @@ import { useAlternateUrls } from '@/metadata/useAlternateUrls'
 import { useCanonicalUrl } from '@/metadata/useCanonicalUrl'
 import { routes } from '@/navigation/routes.config'
 import { PostsList } from '@/post/PostsList'
+import { createUrl } from '@/utils/createUrl'
 import { getFullName } from '@/utils/getFullName'
 
 const pageSize = 12
@@ -122,40 +128,88 @@ export default function AuthorPage(props: AuthorPageProps): JSX.Element {
         canonicalUrl={canonicalUrl}
         languageAlternates={languageAlternates}
       />
-      <PageContent className="max-w-screen-xl px-10 py-16 mx-auto space-y-10 w-full">
-        <h1 className="text-4.5xl font-bold text-center">{fullName}</h1>
-        <p className="text-center text-neutral-500 text-lg">
+      <PageContent className="w-full max-w-screen-xl px-10 py-16 mx-auto space-y-10">
+        <div className="flex items-center justify-center space-x-4">
+          {author.avatar != null ? (
+            <img
+              src={author.avatar}
+              alt=""
+              loading="lazy"
+              className="object-cover w-16 h-16 border-2 rounded-full border-primary-600"
+              width={64}
+              height={64}
+            />
+          ) : null}
+          <h1 className="text-4.5xl font-bold text-center">{fullName}</h1>
+        </div>
+        <p className="text-lg text-center text-neutral-500">
           {author.description}
         </p>
-        <dl>
+        <dl className="flex items-center justify-center space-x-4 font-medium text-neutral-500">
           {author.email !== undefined ? (
             <div>
-              <dt>Email</dt>
-              <dd>{author.email}</dd>
+              <dt className="sr-only">Email</dt>
+              <dd>
+                <a
+                  href={`mailto:${author.email}`}
+                  className="flex items-center space-x-1"
+                >
+                  <Icon icon={EmailIcon} className="w-4 h-4" />
+                  <span>{author.email}</span>
+                </a>
+              </dd>
             </div>
           ) : null}
           {author.website !== undefined ? (
             <div>
-              <dt>Website</dt>
-              <dd>{author.website}</dd>
+              <dt className="sr-only">Website</dt>
+              <dd>
+                <a
+                  href={author.website}
+                  className="flex items-center space-x-1"
+                >
+                  <Icon icon={GlobeIcon} className="w-4 h-4" />
+                  <span>{author.website}</span>
+                </a>
+              </dd>
             </div>
           ) : null}
           {author.twitter !== undefined ? (
             <div>
-              <dt>Twitter</dt>
-              <dd>{author.twitter}</dd>
+              <dt className="sr-only">Twitter</dt>
+              <dd>
+                <a
+                  href={String(
+                    createUrl({
+                      path: author.twitter,
+                      baseUrl: 'https://twitter.com',
+                    }),
+                  )}
+                  className="flex items-center space-x-1"
+                >
+                  <Icon icon={TwitterIcon} className="w-4 h-4" />
+                  <span>{author.twitter}</span>
+                </a>
+              </dd>
             </div>
           ) : null}
           {author.orcid !== undefined ? (
             <div>
-              <dt>ORCID</dt>
-              <dd>{author.orcid}</dd>
-            </div>
-          ) : null}
-          {author.linkedin !== undefined ? (
-            <div>
-              <dt>LinkedIn</dt>
-              <dd>{author.linkedin}</dd>
+              <dt className="sr-only">ORCID</dt>
+              <dd>
+                <a
+                  href={String(
+                    createUrl({
+                      path: author.orcid,
+                      baseUrl: 'https://orcid.org',
+                    }),
+                  )}
+                  className="flex items-center space-x-1"
+                >
+                  <Icon icon={OrcidIcon} className="w-4 h-4" />
+                  <span>{author.orcid}</span>
+                </a>
+              </dd>
             </div>
           ) : null}
         </dl>
